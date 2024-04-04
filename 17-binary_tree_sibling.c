eb9e16d2689d29852260c2553dfcaa769777b524
Anto Vega <antovega@Antos-MacBook-Pro-13.local>
2024-04-04 12:15:03 -0400

    binarytreesibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,10 +7,12 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	binary_tree_t *parent;
+
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *parent = node->parent;
+	parent = node->parent;
 
 	if (parent->left != NULL && parent->left != node)
 		return (parent->left);
