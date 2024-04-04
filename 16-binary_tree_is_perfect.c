67556a9a1c41f482fc78a4cf0449741652fd36d5
Anto Vega <antovega@Antos-MacBook-Pro-13.local>
2024-04-04 12:48:08 -0400

    binarytreeisprefect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -44,6 +44,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree->left || !tree->right)
 		return (0);
 
-	return (binary_tree_is_perfect(tree->left) && 
+	return (binary_tree_is_perfect(tree->left) &&
 			binary_tree_is_perfect(tree->right));
 }
